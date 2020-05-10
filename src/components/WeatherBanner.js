06424b4a2f0da991ce7ce795e6501fa9b6a07510
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-05-10 09:56:38 +0200

    Add files via upload

---
+++ src/components/WeatherBanner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WeatherBanner = () => (
+
+    <div id="weatherBanner" className="ui segment image">
+        <a href="https://tom2kota.github.io/current-weather-js/">
+            <img src="https://via.placeholder.com/468x60?text=Visit+Weather+Now" alt="weather banner"/>
+        </a>
+    </div>)
+
+export default WeatherBanner
+
+
