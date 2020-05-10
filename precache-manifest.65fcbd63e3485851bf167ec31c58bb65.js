482d1199069bfcab31c525eacf11096e12eff9ea
tom2kota <tom2kota@gmail.com>
2020-05-10 09:25:56 +0000

    Updates

---
+++ precache-manifest.65fcbd63e3485851bf167ec31c58bb65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5652eaffe6b4ee3bff39b4683e793ad5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37ed5a04bd49702d19ac",
+    "url": "./static/css/main.98c9c6b7.chunk.css"
+  },
+  {
+    "revision": "f576aad882532dd19962",
+    "url": "./static/js/2.bd2fa1a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bd2fa1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ed5a04bd49702d19ac",
+    "url": "./static/js/main.e975273a.chunk.js"
+  },
+  {
+    "revision": "1374192660583077edc5",
+    "url": "./static/js/runtime-main.caf2e88a.js"
+  },
+  {
+    "revision": "19461b5bfc69ee6c637d55f7f5932a4f",
+    "url": "./static/media/weather-logo.19461b5b.svg"
+  }
+]);
